1b2aa071afa47cbd616a8092de8b4456d4829f3a
Vickey Shrestha <logontovickey@hotmail.com>
2021-02-01 20:41:42 -0600

    - code cleanup

--- drivers/sql/sql.go
+++ drivers/sql/sql.go
@@ -1,35 +1,12 @@
 package sql
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
 )
 
-type Driver interface {
-	Insert(context.Context, Rows, ...Field) string
-	Query(context.Context, string, ...interface{}) (Rows, error)
-	Exec(context.Context, string, ...interface{}) (int64, error)
-	Delete(context.Context, string, string, string) (int64, error)
-	Update(context.Context, ...string) (int64, error)
-}
-
-type Datatype string
-
-type Rows struct {
-	Schema string
-	Table  string
-	Fields []Field
-	Values [][]interface{}
-}
-
-type Field struct {
-	Datatype
-	Name string
-}
-
-func CreateDbConnection(host, user, password, dbName string, port int) (Driver, error) {
+func NewPostgresDbConnection(host, user, password, dbName string, port int) (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbName)
@@ -46,6 +23,5 @@ func CreateDbConnection(host, user, password, dbName string, port int) (Driver, 
 
 	fmt.Println("Successfully connected!")
 
-	// TODO: return the driver
-	return nil, nil
+	return db, nil
 }
